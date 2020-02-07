c06f364efd16d899519d1d2df82ec377e734f79a
Phillip Pascual <phillip.pascual@gmail.com>
2020-02-07 16:20:02 -0600

    Added documentation for UserData class.

--- src/main/java/com/phillippascual/data/UserData.java
+++ src/main/java/com/phillippascual/data/UserData.java
@@ -22,6 +22,11 @@ public class UserData {
 
 	private static ArrayList<User> users = new ArrayList<User>();
 
+	/*
+	 * The save() method takes in no parameters.  It instantiates a FileOutputStream and ObjectOutputStream object, and uses
+	 * these objects to write all User objects in ArrayList 'user' to the 'logins.ser' file.  Once all objects are written
+	 * it closes both streams.  Any IOExceptions are thrown to the calling method/class for handling.
+	 */
 	public static void save() throws IOException {
 		FileOutputStream fileOut = new FileOutputStream("logins.ser");
 		ObjectOutputStream outputStream = new ObjectOutputStream(fileOut);
@@ -31,6 +36,12 @@ public class UserData {
 		log.debug("Users saved.");
 	}
 
+	/*
+	 * The load() method takes in no parameters.  It instantiates a FileInputStream and ObjectInputStream, and uses these
+	 * objects to load all User objects in file 'logins.ser' and add them to the 'users' ArrayList.  Once all User objects
+	 * are read from the file and added to 'users', it closes both stream objects.  Any IOExceptions are thrown to the
+	 * calling method/class for handling.
+	 */
 	@SuppressWarnings("unchecked")
 	public static void load() throws IOException {
 		FileInputStream fileIn = new FileInputStream("logins.ser");
@@ -46,6 +57,11 @@ public class UserData {
 		fileIn.close();
 	}
 
+	/*
+	 * The addUser() method takes in a User object as a parameter.  It adds the passed-in User object to the 'users'
+	 * ArrayList, and then invokes the save() method to save the ArrayList to the 'logins.ser' file.  Any IOException
+	 * thrown by the save() method is caught and handled in this method.
+	 */
 	public static void addUser(User user) {
 		users.add(user);
 		log.debug("User added.");
@@ -56,6 +72,13 @@ public class UserData {
 		}
 	}
 
+	/*
+	 * The validateLogin() method takes in two parameters, username and password both of type String.  It iterates through 
+	 * the 'users' ArrayList, and if it finds a User object with username matching the passed-in username, password 
+	 * matching the passed-in password.  If the employee field is true, the method returns true.  If false, the method
+	 * returns false.  If no user with matching username and/or password is found in the 'users' ArrayList, it notifies
+	 * the user and returns them to the login menu.
+	 */
 	public static boolean validateLogin(String username, String password) {
 		for (User u : users) {
 			if (u.getUsername().equals(username) && u.getPassword().equals(password) && u.isEmployee()) {
@@ -72,6 +95,11 @@ public class UserData {
 
 	}
 
+	/*
+	 * The listUsers() method takes in no parameters.  It iterates through the 'users' ArrayList and prints the fields
+	 * of each User object in the ArrayList to the user.  Since this is admin-only functionality, once the iteration is
+	 * complete it returns the user to the Administrator menu.
+	 */
 	public static void listUsers() {
 		System.out.println("User list:");
 		System.out.println("----------");
@@ -83,6 +111,9 @@ public class UserData {
 		Employee.adminMenu();
 	}
 	
+	/*
+	 * The getUsers() function takes no parameters.  It returns ArrayList 'users'.
+	 */
 	public static ArrayList<User> getUsers() {
 		return users;
 	}
